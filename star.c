3d56e0c8c8c71d5322f3fa0ad5a0694c1078660b
zhanghanyu <zhanghanyu@bbktel.com>
2017-10-20 10:59:59 +0800

    the brihtest star commint second

---
+++ star.c
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  star.c
+ *
+ *    Description:  夜空中最亮的星
+ *
+ *        Version:  1.0
+ *        Created:  2017年10月20日 10时58分25秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (zhanghanyu) 
+ *        Company:  vivo
+ *
+ * =====================================================================================
+ */
+
+
+The brightest star in the night sky
+夜空中最亮的星
+The brightest star in the night sky can hear
+夜空中最亮的星 能否听清
+That looking at the heart of loneliness and sigh
+那仰望的人 心底的孤独和叹息
+The brightest star in the night sky can remember
+夜空中最亮的星 能否记起
+Once with my colleagues disappeared in the wind shadow
+曾与我同行 消失在风里的身影
+I pray to have a clear mind
+我祈祷拥有一颗透明的心灵
+And tears in the eye
+和会流泪的眼睛
+Give me the courage to believe
+给我再去相信的勇气
