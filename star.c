0fe5a46558a7634656e7b0c1d363ecac0916a448
zhanghanyu <zhanghanyu@bbktel.com>
2017-10-20 10:55:54 +0800

    the brihtest star in the night sky

---
+++ star.c
@@ -0,0 +1,66 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  star.c
+ *
+ *    Description:  夜空中最亮的星
+ *
+ *        Version:  1.0
+ *        Created:  2017年10月20日 10时53分31秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (张晗玉), 
+ *        Company:  vivo
+ *
+ * =====================================================================================
+ */
+
+The brightest star in the night sky
+夜空中最亮的星
+The brightest star in the night sky can hear
+夜空中最亮的星 能否听清
+That looking at the heart of loneliness and sigh
+那仰望的人 心底的孤独和叹息
+The brightest star in the night sky can remember
+夜空中最亮的星 能否记起
+Once with my colleagues disappeared in the wind shadow
+曾与我同行 消失在风里的身影
+I pray to have a clear mind
+我祈祷拥有一颗透明的心灵
+And tears in the eye
+和会流泪的眼睛
+Give me the courage to believe
+给我再去相信的勇气
+Beyond lies to hug you
+越过谎言去拥抱你
+When I can't find the meaning of existence
+每当我找不到存在的意义
+Whenever I get lost in the darkness of night
+每当我迷失在黑夜里
+The brightest star in the night sky
+夜空中最亮的星
+Please let me go out
+请指引我走出去
+The brightest star in the night sky are know
+夜空中最亮的星 是否知道
+That I have concentric figure now in where
+那曾与我同心的身影 如今在哪里
+The brightest star in the night sky are concerned
+夜空中最亮的星 是否在意
+The sun rises or accidental first come first
+是太阳先升起 还是意外先来临
+I would rather all the pain in my heart
+我宁愿所有痛苦都留在心里
+Don't forget your eyes
+也不愿忘记你的眼睛
+Give me the courage to believe
+给我再去相信的勇气
+Beyond lies to hug you
+越过谎言去拥抱你
+When I can't find the meaning of existence
+每当我找不到存在的意义
+Whenever I get lost in the darkness of night
+每当我迷失在黑夜里
+The brightest star in the night sky please light me
+夜空中最亮的星 请照亮我前行
